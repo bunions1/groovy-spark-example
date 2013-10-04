e316c7fecceed86f5c08f7fb66b78d04270e6dae
Aaron Babcock <aaron.babcock@optimine.com>
2013-10-04 15:15:35 -0500

    checkpoint

--- src/main/groovy/Example.groovy
+++ src/main/groovy/Example.groovy
@@ -3,40 +3,103 @@ import static repl.Repl.*;
 import scala.Tuple2;
 import repl.F;
 import repl.PairF;
+import categories.RddMethods
 
 
 
 class Example{
 
 
-
 	public static def sc;
 	public static def classServer;
-	static {
+	public static void init(){
 		classServer = new spark.HttpServer(new java.io.File("/tmp"))
 		classServer.start()
 		System.setProperty("spark.repl.class.uri", classServer.uri())
-		sc  = new JavaSparkContext("spark://sparkmaster:7077", "groovySpark", "/home/appadmin/spark", 
+		//local[1]
+		sc  = new JavaSparkContext("spark://lubuntu64-1.optimine.local:7077", "groovySpark", "/home/appadmin/spark", 
 								   [
 									   "target/libs/groovy-spark-example-2.2.0-SNAPSHOT.jar",
 									   "libs/repl.jar",
 									   "libs/groovy-all-2.2.0-SNAPSHOT.jar",
 								   ]		 as String[]);
+
+	}
+		
+	static def squaredDist(vecOne, vecTwo) {
+		return [vecOne,vecTwo].transpose().collect{col -> (col[0] - col[1])**2 }.sum()
 	}
 
-		
-
-
+	static def testMapScope  = new F({ ps ->  
+										//columnwise average, transpose is zip in the groovy world
+										def tmp = ps.transpose()*.sum().collect{ (it / ps.size()) }
+										tmp.trimToSize()
+									 })
 
 	static void main(String[] args){
+		println(new Date())
+		//		repl()
+		init()
+		use(RddMethods){
+			def textFile = sc.textFile("hdfs://lubuntu64-1.optimine.local:8020/tmp/kmeans_data.txt")
+			def K = 2;
+			double convergeDist = 0.5
+			println("nano****************************************")			
+			//parse text vectors to rdd
+			def data = textFile.collect{ line ->
+				def tmp = line.split(" ").collect{it.toDouble()}
+				tmp.trimToSize()
+				return tmp
 
-		def textFile = sc.textFile("hdfs://namenode.local:8020/test.csv")
-		def result = textFile.map(new PairF({ row ->
-			return new Tuple2(row, Arrays.asList([1,2,3]))
+				//				double[] doubleLine =  line.split(" ").collect{it.toDouble()} as double[]
+				//				return doubleLine
+			}.cache()
+			println("0****************************************")			
+			def centroids = data.takeSample(false, K, 42)
+			def tempDist;
+			println("1****************************************")			
+			while(true){
+				println("2****************************************")			
+				//create pair rdd keyed by the index of the centroid that each vec is closes to
+				def closest = data.collectEntries{ row ->
+					def distances = centroids.collect{ c -> squaredDist(row, c) }
+					[distances.indexOf(distances.min()), row]
+				}
+				println("3****************************************")			
 
-        }))
-		println(result.count())
-		repl()
+				def pointsGroup = closest.groupByKey()
+			/*
+			def pointsStats = closest.reduceByKey(new F2({ vecOne, vecTwo ->  
+				                                             def tmp = [vecOne, vecTwo].transpose()*.sum()
+														 })
+				)
+			*/
+				repl()
+				//calc new centers
+				def newCentroids = pointsGroup.mapValues(testMapScope).collectAsMap();
+			println("4****************************************")			
+
+				//distance between old and new centers
+				tempDist = 0.0
+				centroids.eachWithIndex{c, i -> 
+					tempDist += squaredDist(c, newCentroids[i])
+				}
+
+				//copy from map to array
+				newCentroids.each{k, v -> 
+					centroids[k] = v 
+				}
+			println("5****************************************")			
+				println("Finished iteration (delta = " + tempDist + ")");
+				if(!(tempDist > convergeDist))
+					break;
+			}
+
+			System.out.println("Final centers:");
+			centroids.each{println(it)}
+			println(new Date())
+			//			repl()
+		}
 	}
 
 }
